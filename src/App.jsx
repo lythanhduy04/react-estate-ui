16ffb8865e7777190f3c4b0dd125310cce0ded4e
ottert <thanhduyly18@gmail.com>
2024-08-26 22:06:44 +0700

    build button buy and rent use useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./layout.scss";
 import Navbar from "./components/navbar/Navbar";
+import HomePage from "./router/homePage/homePage";
 function App() {
   return (
     <div class="layout">
-      <Navbar />
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
     </div>
   );
 }
