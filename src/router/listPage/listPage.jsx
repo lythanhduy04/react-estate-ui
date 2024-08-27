7dbc2ce4782773ef35f569b351b99d72bb94e315
Otter <thanhduyly18@gmai.com>
2024-08-27 17:29:30 +0700

    code list page

--- src/router/listPage/listPage.jsx
+++ src/router/listPage/listPage.jsx
@@ -1,7 +1,19 @@
+import Filter from "../../components/filter/Filter";
 import "./listPage.scss";
+import { listData } from "../../lib/dummydata";
 
 function ListPage() {
-  return <div className="listPage">ListPage</div>;
+  const data = listData;
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+        </div>
+      </div>
+      <div className="mapContainer">Map</div>
+    </div>
+  );
 }
 
 export default ListPage;
