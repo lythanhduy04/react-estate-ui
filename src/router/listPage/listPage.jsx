31b81e0641ce6d8ab08c08687331a702ff722fcd
Ly Thanh Duy <thanhduyly18@gmail.com>
2024-08-28 06:57:26 +0700

    css filter

--- src/router/listPage/listPage.jsx
+++ src/router/listPage/listPage.jsx
@@ -1,6 +1,7 @@
 import Filter from "../../components/filter/Filter";
 import "./listPage.scss";
 import { listData } from "../../lib/dummydata";
+import Card from "../../components/card/card";
 
 function ListPage() {
   const data = listData;
@@ -9,6 +10,9 @@ function ListPage() {
       <div className="listContainer">
         <div className="wrapper">
           <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
         </div>
       </div>
       <div className="mapContainer">Map</div>
