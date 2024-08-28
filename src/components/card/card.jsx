df554438dd8e035e8192bb4a19e81ed62233b49c
Otter <thanhduyly18@gmai.com>
2024-08-28 11:23:17 +0700

    xong cơ bản của list card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom";
 import "./card.scss";
 
-function Card() {
-  return <div className="card">Card</div>;
+function Card({ item }) {
+  return (
+    <div className="card">
+      <Link to={`${item.id}`} className="imgContainer">
+        <img src={item.img} alt="img" />
+      </Link>
+      <div className="textContainer">
+        <h2 className="title">
+          <Link to={`${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className="address">
+          <img src="/pin.png" alt="" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">${item.price}</p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src="/bed.png" alt="bed" />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+            <div className="feature">
+              <img src="/bath.png" alt="bath" />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src="/save.png" alt="save" />
+            </div>
+            <div className="icon">
+              <img src="/chat.png" alt="save" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
