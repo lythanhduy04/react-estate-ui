c917e5a2a98bc8b92f3c52427ef04b7b67cca022
Ly Thanh Duy <thanhduyly18@gmail.com>
2024-08-30 04:43:35 +0700

    finish slider bar and base singlePage.jsx

--- src/router/singlePage/singlePage.jsx
+++ src/router/singlePage/singlePage.jsx
@@ -1,11 +1,35 @@
+import Slider from "../../components/slider/Slider";
 import "./singlePage.scss";
+import { singlePostData, userData } from "../../lib/dummydata";
 
 function SinglePage() {
   return (
     <div className="singlePage">
-      singdle
-      <div className="details"></div>
-      <div className="features"></div>
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="adress">
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">{singlePostData.description}</div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
     </div>
   );
 }
